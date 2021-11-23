949888675ce1ad74e060b430e3c4bbdf37e4276a
2299112@gmail.com <2299112@gmail.com>
2021-11-24 00:41:11 +0500

    Добавьте файлы проекта.

---
+++ Lab7.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, v, p;
+            x = Convert.ToDouble(Console.ReadLine());
+            if (x >= 0)
+            {
+                Cube(x, out v, out p);
+                Console.WriteLine("Площади поверхности куба - {0}, Объем куба - {1}", p, v);
+            }
+            else
+            {
+                Console.WriteLine("Введено неверное значение");
+            }
+            Console.ReadKey();
+
+        }
+        //Метод расчет параметров куба
+        static void Cube(double x, out double v, out double p)
+        {
+            p = x * x * 6;
+            v = x * x * x;
+        }
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сравнение площадей треугольников
+            Console.WriteLine("Введите длину сторон первого треугольника");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+            double s1 = TriangleS(x, y, z);
+
+            Console.WriteLine("Введите длину сторон второго треугольника");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            z = Convert.ToDouble(Console.ReadLine());
+            double s2 = TriangleS(x, y, z);
+
+            if (s1 > s2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше площади второго треугольника");
+            }
+            else
+            {
+                if (s1 < s2)
+                {
+                    Console.WriteLine("Площадь второго треугольника больше площади первого треугольника");
+                }
+                else
+                {
+                    Console.WriteLine("Площади треугольников равны");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static double TriangleS(double x, double y, double z)
+        {
+            double p = (x + y + z) / 2;
+            double S = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return S;
+        }
+
+
+
+    }
+}
